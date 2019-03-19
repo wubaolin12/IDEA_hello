ba64c33cedc4be4d7740b66ee869701f8a5b50de
wubaolin12 <494899421@qq.com>
2019-03-19 10:17:54 +0800

    “注释”

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,39 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 测试一个IDEA的web项目;采用 Servlet 3.0框架
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //获取当前日期
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentDate = df.format(new Date());
+
+        req.setAttribute("currentDate", currentDate);
+        System.out.println("我要跳转到jsp---");
+        req.getRequestDispatcher("/jsp/hello.jsp")
+                .forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+
+    }
+
+
+}
